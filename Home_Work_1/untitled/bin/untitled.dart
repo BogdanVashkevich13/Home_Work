fa5e1dca4389ecc5b8b0204808231f9c3c7d1778
BogdanVashkevich13 <105815331+BogdanVashkevich13@users.noreply.github.com>
2022-05-18 21:30:06 +0300

    Add files via upload

---
+++ Home_Work_1/untitled/bin/untitled.dart
@@ -0,0 +1,44 @@
+import 'dart:collection';
+
+void main(List<String> arguments) {
+  print('Hello world!');
+  double Point = 4.5;
+  Point++;
+  print(Point);
+  var nums = 'Alex Go Home';
+  print(nums);
+  //задание 1:
+  //if (nums[0].toUpperCase() == nums[0]) {
+  //print('true');
+  //} else {
+  //print('false');
+  //}
+//Задание 2:
+  //var text = 'Alex GO HoMe';
+//for(text; i = 0; i < text.length; i++);{
+  //print (text);
+
+//задание 3
+//var nim = 'ALEX go HoMe';
+  //print (nim.toUpperCase());
+  // }
+  // ignore: unrelated_type_equality_checks
+  //if (nums.toUpperCase() == nums.length)
+    //{
+    //print(nums.toLowerCase());
+  //} else if (nums.toLowerCase() == nums.length) {
+    //print(nums.toUpperCase());
+  //}
+
+//задание 4
+//задание 5
+  // var example = new HashMap();
+  //example ['x'] = 5.5;
+  //example ['y'] = 7;
+  //print({example['x']*example['x']*((example['y']-8)/5)-(((example['x']*2)+5)/example['y'])});
+}
+
+
+
+
+
